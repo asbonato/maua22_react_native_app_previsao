646f6a56aa6f1ff5e87d10051870bd066a8e98b3
asbonato <asbonato@gmail.com>
2022-10-30 13:14:07 -0300

    Apostila 6 completa

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// module.exports = function(api) {
+//   api.cache(true);
+//   return {
+//     presets: ['babel-preset-expo','react-native-dotenv'],
+//   };
+// };
+// module.exports = {
+//   presets: [
+//      'module:metro-react-native-babel-preset',
+//      'module:react-native-dotenv'
+//   ]
+// }
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    ["module:react-native-dotenv", {
+      "envName": "APP_ENV",
+      "moduleName": "@env",
+      "path": ".env",
+      "safe": false,
+      "allowUndefined": true,
+      "verbose": false
+    }]
+  ]
 };
+
